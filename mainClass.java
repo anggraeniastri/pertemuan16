38b982a1b5515874508b150ebb32cf7468159dad
anggraeniastri <astrianggraeni22@yahoo.co.id>
2017-06-07 23:36:52 +0700

    Add files via upload

---
+++ mainClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASTRI
+ */
+public class mainClass {
+    
+    public static void main (String[]args){
+        Tas<String>namaString = new  Tas<>();
+        namaString.setNama("Tas Roxy");
+        System.out.println(namaString.getNama());
+        
+    }
+    
+}
---
+++ tas.java
@@ -0,0 +1,15 @@
+
+public class tas <T> {
+    private T nama;
+    
+    public T getType(){
+        return nama;
+    }
+    
+    public void setNama(T nama){
+        this.nama = nama; 
+    }
+    
+    
+    
+}
